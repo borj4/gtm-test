137a22a01a7d36ff9a44313324f3a084d9d90dff
borj4 <river4car@gmail.com>
2022-04-06 15:44:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import TagManager from 'react-gtm-module/dist/TagManager';
+// import TagManager from 'react-gtm-module/dist/TagManager';
 import './App.css';
 import { Accordion } from './components/Accordion/Acordion';
 import { Header } from './components/Header/Header';
 import { Youtube } from './components/Video/Video';
 
-const tagManagerArgs = {
-  gtmId: 'GTM-W4LSZWJ'
-}
+// const tagManagerArgs = {
+//   gtmId: 'GTM-W4LSZWJ'
+// }
 
-TagManager.initialize(tagManagerArgs);
+// TagManager.initialize(tagManagerArgs);
 
 const App =()=>  {
   const [cart, setCard] = useState([]);
