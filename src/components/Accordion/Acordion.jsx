a3520a97b919708669fe4384c3252329cfa47608
borj4 <river4car@gmail.com>
2022-04-07 19:33:27 +0200

    adding: cart state

--- src/components/Accordion/Acordion.jsx
+++ src/components/Accordion/Acordion.jsx
@@ -5,9 +5,38 @@ import "./styles.css";
 export function Accordion (props) {
   // State to show/hide accordion
   const [show, setShow] = useState(false);
+  
   const handleOpen = () => {
     setShow(!show);
   };
+  
+  const [cart, setCart] = useState([]);
+  const itemsToBuy = (element, index) => {
+    // si el elemento clicado existe en el carrito...
+    let oneMore = cart;
+    if (cart.some(e=>e.name===element.name)) { ////// el fallo está aquí, en cómo se evalúa el if. Volveré.
+      // me copias el estado
+      // lo recorres
+      cart.map((f,i,array)=>{
+        // si el elemento clicado coincide con el que estoy iterando...
+        if (f.name===element.name) {
+          // sumas uno a su propiedad de cantidad
+          oneMore[i].amount++;
+          console.log(array);
+          // y me lo guardas la copia actualizada en el estado, pisando lo anterior
+        }
+        return [];
+      })
+      setCart(oneMore);
+
+    }
+    else {
+      const newProduct = products[index];
+      newProduct.amount = 1;
+      setCart([...cart, newProduct]);
+    }
+  };
+
 
   return (
     <div id="contenedor-accordian" className="accContainer">
@@ -18,12 +47,13 @@ export function Accordion (props) {
           </div>
             {show && (
               <div className="accordian-body">
-                {products.map(({name},i)=>{return (<button 
-                                                    className="accButton"
-                                                    key={i}
-                                                    id={`product-${i}`}>
-                                                    {name}
-                                                  </button>)
+                {products.map((e,i)=>{return  <button 
+                                                      onClick={()=>{itemsToBuy(e,i)}}
+                                                      className="accButton"
+                                                      key={i}
+                                                      id={`product-${i}`}>
+                                                      {e.name}
+                                                    </button>
                 })}
               </div>
             )}
