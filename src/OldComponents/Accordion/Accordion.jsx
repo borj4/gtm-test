a6f4ec47e7b0d425f6612ef34e0481af5f2becd8
borj4 <river4car@gmail.com>
2022-05-04 17:19:51 +0200

    add: new accordion

--- src/OldComponents/Accordion/Accordion.jsx
+++ src/OldComponents/Accordion/Accordion.jsx
@@ -1,52 +1,32 @@
 import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion'; // https://github.com/springload/react-accessible-accordion
 import "./styles.css";
 // import { products } from "../../utils/data";
 
 const Accordion = ({value}) => {
   // State to show/hide accordion
-  const [show, setShow] = useState(false);
-  
-  // const {itemsToBuy, preCart, setCart } = value;
-
-  
-  const handleOpen = () => {
-    setShow(!show);
-  };
-  
-
-  // const addToCart = () => {
-  //   setCart(...preCart)
-  // };
 
   return (
-    // <div>Heeeeeeeeeeeey soy un acoooordeoooon</div>
-    <div id="contenedor-accordian" className="accContainer">
-      <div id="accordian" className="accordian">
-        <div id="accordian-header" className="accordian-header" onClick={handleOpen}>
-          <div id="">¿Qué quieres comer hoy?</div>
-          <div className="sign">{show ? '-' : '+'}</div>
-          </div>
-            {/* {show && (
-              <div className="accordian-body">
-                {products.map((e,i)=>{return  <div 
-                                                className="accButton"
-                                                key={i}
-                                                id={`product-${i}`}>
-                                                <p> {e.name} </p>
-                                                <button onClick={()=>{itemsToBuy(true,e,i,products)}}> + </button>
-                                                <p>
-                                                  { preCart.length > 0 ? 
-                                                    preCart.map(search=>{ if (search.name===e.name){ return search.amount } else return 0 }) :
-                                                    0 }
-                                                </p>
-                                                <button onClick={()=>{itemsToBuy(false,e,i,products)}}> - </button>
-                                              </div>
-                })}
-              <button id="addToCart" onClick={addToCart}>Añadir e ir al carrito</button> 
-              </div>
-            )} */}
-          </div>
-    </div>
+    <Accordion allowZeroExpanded>
+        {items.map((item) => ( // psssst! items dont exists yet!!!
+            <AccordionItem key={item.uuid}>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        {item.heading}
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  {item.content}
+                </AccordionItemPanel>
+            </AccordionItem>
+        ))}
+    </Accordion>
   );
 }
 export default Accordion
